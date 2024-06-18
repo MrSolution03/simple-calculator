e6f56230614b64e507b4686e76d4741ce73b2eaa
Tech_Solution <163984462+MrSolution03@users.noreply.github.com>
2024-06-18 17:38:15 +0300

    simple calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function Calculator() {
+  const [input,setInput]=useState('')
+  const [history, setHistory] = useState([])
+
+  const handleInputValue = (value)=>{
+    setInput(input+value);
+  }
+
+  const handleClear = ()=>{
+    setInput('');
+  }
+
+  const handleEqual = ()=>{
+    try{
+      const result = eval(input);
+      setHistory([...history, `${input}=${result}`])
+      setInput(result.toString());
+    }catch{
+      setInput('Error');
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-96 mx-auto'>
+      <div class="w-full">
+        <input
+        type="text"
+        value={input}
+        className="w-full focus:outline-none bg-gray-300 p-4 text-right text-2xl"
+        placeholder='0'
+        readOnly
+        />
+      </div>
+      <div className="grid grid-cols-4 py-2"> 
+        {['7','8','9','/','4','5','6','*','1','2','3','-','0','.','=','+'].map((char)=>(
+          <button key={char} onClick={()=> char === '=' ? handleEqual():handleInputValue(char)} className="bg-blue-500 text-white p-4 rounded m-2 focus:outline-none">
+            {char}
+          </button>
+        ))}
+        <button onClick={handleClear} className='col-span-2 bg-red-500 text-white p-4 rounded focus:outline-none'>
+          C
+        </button>
+      </div>
+      <div className='mt-4 bg-white p-4 rounded'>
+        <h2 className="text-xl mb-2">History</h2>
+        <ul className="list-disc list-inside">
+          {history.map((item, index)=>(
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Calculator;
